3ef08c6d42e5901a107636bd373876225b8e6211
Selim Şahin <selimsahin.sns@gmail.com>
2024-01-04 20:01:38 +0300

    applied gaussian blur and applied gaussian blur and added export of resulting images

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 # Read the puppy.jpeg image from the 'images' folder
 img = cv2.imread('images/puppy.jpeg')
@@ -14,13 +15,24 @@ x = np.arange(256)
 table = np.interp(x, xp, fp).astype('uint8')
 
 # Apply contrast stretching
-img = cv2.LUT(img, table)
+contrast_stretching = cv2.LUT(img, table)
 
+# Apply the GaussianBlur filter
+gaussian_blur = cv2.GaussianBlur(img, (5, 5), 0)
+
+# Show the images
 cv2.imshow("Original Image", original)
-cv2.imshow("Output", img)
+cv2.imshow("Contrast Stretching", contrast_stretching)
+cv2.imshow("Gaussian Blur", gaussian_blur)
 
+# Draw all in one window and label them
+cv2.imshow("All", np.hstack([original, contrast_stretching, gaussian_blur]))
+
+# Save all of them in a folder named 'outputs'
+cv2.imwrite("outputs/contrast_stretching.jpg", contrast_stretching)
+cv2.imwrite("outputs/gaussian_blur.jpg", gaussian_blur)
+cv2.imwrite("outputs/all.jpg", np.hstack([original, contrast_stretching, gaussian_blur]))
+
+# Wait for any key to be pressed before closing the image
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-# Wait for any key to be pressed
-cv2.waitKey()
